e895626503a435838c335fe6a9c191837a725c15
Vijay Kumar <contactvkr@yahoo.com>
2022-09-04 17:43:38 +0530

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -4,7 +4,7 @@ let player = document.getElementById("player");
 let close = document.getElementById("close");
 let loading = document.getElementById("loading");
 let iframeBox = document.getElementById("iframeBox");
-const cors = "https://cors-anywhere.herokuapp.com/";
+const cors = "https://corsytvkr.herokuapp.com/";
 const channel = "https://theofficialvkr.000webhostapp.com/api/ch.php/?vkr="+channelUrl;
 let jsonCall = new XMLHttpRequest();
 jsonCall.open("GET",cors+channel);
